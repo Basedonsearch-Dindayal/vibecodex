279637a9bb4ccd9b57dfcdfeecd2b41d54491c23
Basedonsearch-Dindayal <dindayalsinha301@gmail.com>
2025-11-18 00:56:12 +0530

    05: ai-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,23 @@
+import {gemini, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world2" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    //Downlaodig a video
-    await step.sleep("wait-a-moment", "30s");
+  async ({ event}) => {
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const summarizer = createAgent({
+      name: "code_agent",
+      system: "You are an expert next.js developer. you write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: gemini({ model: "gemini-2.5-flash"}),
+    });
 
-    //imagine this is a transcript step
-    await step.sleep("wait-a-moment", "10s");
+    const { output } = await summarizer.run(
+    `Write the following snippet: ${event.data.value}`,
+    );
+    console.log(output);
 
-    //imagine this is a summary step
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+
+    return { output};
   },
 );
